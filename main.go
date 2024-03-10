9b6d85ffedda23b13078a26afa307961b6ddcc0d
Paweł Lendzion <pawellendzion01@gmail.com>
2024-03-10 16:21:00 +0100

    Add persisting messages per build

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 	"net/http"
 )
 
+type Messages struct {
+	Author string
+	Msg    string
+}
+
 func main() {
+	var msgs []Messages
+	msgs = append(msgs, Messages{"Jone", "Hi there"})
+
 	publicFs := http.FileServer(http.Dir("public"))
 	http.Handle("/scripts/", publicFs)
 	http.Handle("/styles/", publicFs)
@@ -15,7 +23,7 @@ func main() {
 		tmpl := template.Must(template.ParseGlob("app/templates/pages/chat/*.tmpl"))
 		template.Must(tmpl.ParseGlob("app/templates/layouts/*.tmpl"))
 
-		tmpl.ExecuteTemplate(w, "index.tmpl", nil)
+		tmpl.ExecuteTemplate(w, "index.tmpl", msgs)
 	})
 
 	http.HandleFunc("/send-msg", func(w http.ResponseWriter, r *http.Request) {
@@ -23,9 +31,11 @@ func main() {
 			fmt.Fprintf(w, "Error: %s", err)
 		}
 
+		msgs = append(msgs, Messages{"Me", r.PostForm.Get("msg")})
+
 		fmt.Fprintf(
 			w,
-			"<li class=\"msgs-list__msg-box msgs-list__msg-box--my\">%s</li>",
+			"<li class=\"msgs-list__msg-box msgs-list__msg-box--right\">%s</li>",
 			r.PostForm.Get("msg"),
 		)
 	})
