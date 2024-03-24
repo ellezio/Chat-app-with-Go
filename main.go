e6ddc03704a1f3342b654e697b0ebe96784df063
Paweł Lendzion <pawellendzion01@gmail.com>
2024-03-24 18:55:23 +0100

    Change http/template to templ and add flake

--- main.go
+++ main.go
@@ -1,68 +1,44 @@
 package main
 
 import (
+	"chatting-app/components"
+	"context"
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
-type Message struct {
-	Author string
-	Msg    string
-}
-
 func main() {
-	var msgs []Message
+	var msgs []components.Message
 
 	publicFs := http.FileServer(http.Dir("public"))
 	http.Handle("/scripts/", publicFs)
 	http.Handle("/styles/", publicFs)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		type MessagePageData struct {
-			Message
-			IsMyMsg bool
-		}
-
-		var pageData struct {
-			IsLoggedIn bool
-			Msgs       []MessagePageData
-		}
-
-		cookie, err := r.Cookie("username")
-		pageData.IsLoggedIn = err == nil
-
-		var username string
-		if err == nil {
+		username := ""
+		if cookie, err := r.Cookie("username"); err == nil {
 			username = cookie.Value
 		}
 
-		pageData.Msgs = make([]MessagePageData, len(msgs))
-		for _, msg := range msgs {
-			pageData.Msgs = append(pageData.Msgs, MessagePageData{
-				Message: msg,
-				IsMyMsg: msg.Author == username,
-			})
-		}
-
-		tmpl := template.Must(template.ParseGlob("app/templates/pages/chat/*.tmpl"))
-		template.Must(tmpl.ParseGlob("app/templates/layouts/*.tmpl"))
-		tmpl.ExecuteTemplate(w, "index.tmpl", pageData)
+		ctx := context.WithValue(r.Context(), "username", username)
+		components.Page(msgs).Render(ctx, w)
 	})
 
 	http.HandleFunc("/send-msg", func(w http.ResponseWriter, r *http.Request) {
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "Error: %s", err)
+		if cookie, err := r.Cookie("username"); err == nil {
+			if err := r.ParseForm(); err != nil {
+				fmt.Fprintf(w, "Error: %s", err)
+			}
+
+			msg := components.Message{
+				Author:  cookie.Value,
+				Content: r.PostForm.Get("msg"),
+			}
+			msgs = append(msgs, msg)
+
+			ctx := context.WithValue(r.Context(), "username", cookie.Value)
+			components.MessageBox(msg).Render(ctx, w)
 		}
-
-		cookie, _ := r.Cookie("username")
-		msgs = append(msgs, Message{cookie.Value, r.PostForm.Get("msg")})
-
-		fmt.Fprintf(
-			w,
-			"<li class=\"msgs-list__msg-box msgs-list__msg-box--right\">%s</li>",
-			r.PostForm.Get("msg"),
-		)
 	})
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
@@ -80,5 +56,9 @@ func main() {
 		http.SetCookie(w, &cookie)
 	})
 
-	http.ListenAndServe(":80", nil)
+	fmt.Println("Start listening on :3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
